ddf5afc23bd25abcde86ad209bcc48e7af52bf11
‘metropolis-long’ <2724785961@qq.com>
2021-08-30 18:55:54 +0800

    FileRename

--- daily/tool/split_words.py
+++ daily/tool/split_words.py
@@ -3,7 +3,7 @@ import os
 import utils
 
 __my_words__ = []
-
+__my_ones_words__ = []
 
 def split_words_dir(dir_path):
     dir = os.listdir(dir_path)
@@ -13,21 +13,46 @@ def split_words_dir(dir_path):
         # 是目录，并且包含今天日期
         if full_path.find(today) < 0 and os.path.isdir(full_path):
             split_words_dir(full_path)
-        else:
+        elif os.path.isdir(full_path):
             ind = d.index(today)
             word = d[ind+len(today):]
             splits = jieba.cut(word)
             __my_words__.append(splits)
-
+        else:
+            pass
 
 def get_word_split_dir():
     split_words_dir(r"D:/data")
     return __my_words__
 
 
+def split_every_word_dir(dir_path):
+    dir = os.listdir(dir_path)
+    today = utils.get_today_str()
+    for d in dir:
+        full_path = os.path.abspath(os.path.join(dir_path, d))
+        # 是目录，并且包含今天日期
+        if full_path.find(today) < 0 and os.path.isdir(full_path):
+            split_every_word_dir(full_path)
+        elif os.path.isdir(full_path):
+            ind = d.index(today)
+            word = d[ind + len(today):]
+            splits = ",".join(word)
+            __my_ones_words__.append(splits)
+        else:
+            pass
+
+
+def get_split_every_word_dir():
+    split_every_word_dir(r"D:/data")
+    return __my_ones_words__
+
+
 if __name__ == "__main__":
     for w in get_word_split_dir():
-        print(", ".join(w))
+        print(",".join(w))
+    for w in get_split_every_word_dir():
+        print(w)
     print('split words over！')
 
 
