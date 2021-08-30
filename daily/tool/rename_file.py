d639951ce4c8167326173550faa148492e407a68
‘metropolis-long’ <2724785961@qq.com>
2021-08-30 18:12:50 +0800

    FileRename

--- daily/tool/rename_file.py
+++ daily/tool/rename_file.py
@@ -33,6 +33,35 @@ class ImageRename():
         print('total %d to rename & converted %d jpgs' % (total_num, i))
 
 
+class FileRename():
+    """
+    图片重命名
+    """
+    def __init__(self,path):
+        # 换成自己图片集所在路径
+        self.path = path
+
+    def rename(self):
+        filelist = os.listdir(self.path)
+        total_num = len(filelist)
+        i = 1
+
+        for item in filelist:
+            # 如果是png就把.jpg改成.png
+            ex = '.wav'
+            if item.endswith('wav'):
+                pass
+            elif item.endswith('.mp3'):
+                ex = '.wav'
+            src = os.path.join(os.path.abspath(self.path), item)
+            # 自己可以更改0的个数
+            dst = os.path.join(os.path.abspath(self.path), '0000' + format(str(i), '0>3s') + ex)
+            os.rename(src, dst)
+            print('converting %s to %s ...' % (src, dst))
+            i = i + 1
+        print('total %d to rename & converted %d jpgs' % (total_num, i))
+
+
 if __name__ == '__main__':
     path =r'D:/data/face_mine/'
     flist = os.listdir(path)
